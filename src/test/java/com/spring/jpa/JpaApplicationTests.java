a3054281a269367d24117857d44471627f36469a
rkritchat <rojanaphruk.r@gmail.com>
2018-03-31 23:12:16 +0700

    Create Unit test

--- src/test/java/com/spring/jpa/JpaApplicationTests.java
+++ src/test/java/com/spring/jpa/JpaApplicationTests.java
@@ -3,18 +3,36 @@ package com.spring.jpa;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class JpaApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
+
 	@Test
-	public void contextLoads() {
+	public void searchUser_byId_eixist_mustFound() throws Exception{
+		mockMvc.perform(get("/user/Kritchat"))
+				.andExpect(jsonPath("id").value("rkritchat"));
+	}
+
+	@Test
+	public void searchUser_byId_noExist_mustNotFound() throws Exception{
+		MvcResult result = mockMvc.perform(get("/user/noExist"))
+				.andReturn();
+
+		assertEquals(result.getResponse().getContentAsString(),"");
 	}
 
 }
